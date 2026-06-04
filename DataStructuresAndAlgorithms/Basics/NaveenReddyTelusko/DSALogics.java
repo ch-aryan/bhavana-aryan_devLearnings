6367448e8d2b3e39658875f2a25fcf001c15cd1a
bhavanach-deve <bhavanachilkeshwaram@gmail.com>
2026-06-04 14:56:26 +0530

    First Collab commit

---
+++ DataStructuresAndAlgorithms/Basics/NaveenReddyTelusko/DSALogics.java
@@ -0,0 +1,9 @@
+package DataStructuresAndAlgorithms.Basics.NaveenReddyTelusko;
+
+public class DSALogics
+{
+    public static void main(String[] args) {
+
+        System.out.println("Hello Team!");
+    }
+}
